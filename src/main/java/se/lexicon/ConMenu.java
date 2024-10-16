cdc5d0ad60e239273e022358f4809e26f79cca39
Bachar <princejackraven@gmail.com>
2024-10-16 10:10:19 +0200

    done

---
+++ src/main/java/se/lexicon/ConMenu.java
@@ -0,0 +1,50 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class ConMenu {
+    public Converter converter=new Converter();
+    public Scanner scanner=new Scanner(System.in);
+
+    public void displayMenu(){
+        boolean isActive = true;
+        while (isActive){
+            System.out.println("**Currency Converter**");
+            System.out.println("choose one of the following operations");
+            System.out.println("1.Sek to Usd");
+            System.out.println("2.Usd to Sek");
+            System.out.println("3.Sek to Euro");
+            System.out.println("4.Euro to Sek");
+            System.out.println("5.Exit");
+
+            int userChoice = validChoice();
+            switch (userChoice){
+                case 1:
+                    converter.convert("sek","USD",0.09);
+                    break;
+                case 2:
+                    converter.convert("USD","Sek",10.42);
+                    break;
+                case 3:
+                    converter.convert("Sek","Eur",0.08);
+                    break;
+                case 4:
+                    converter.convert("Euro","Sek",11.34);
+                    break;
+                case 5:
+                    isActive=false;
+                    System.out.println("Thank you!! bye bye..");
+                    break;
+                default:
+                    System.out.println("invalid choice, enter a number between 1-5");
+            }
+        }
+    }
+    public int validChoice(){
+        while (!scanner.hasNextInt()){
+            System.out.println("Please enter a valid input between 1-5");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
---
+++ src/main/java/se/lexicon/Converter.java
@@ -0,0 +1,31 @@
+package se.lexicon;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Converter {
+    public Scanner scanner = new Scanner(System.in);
+    public void convert(String fromC,String toC,double exRate){
+        double amount = validAmount(fromC);
+        double conAmount = amount * exRate;
+
+        DecimalFormat format= new DecimalFormat("#.##");
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(amount+" "+fromC+" = "+format.format(conAmount)+" "+toC);
+        System.out.println("Done at : "+dateTime);
+    }
+    public double validAmount(String fromC){
+        System.out.println("please enter the amount in "+fromC+":");
+        while (!scanner.hasNextDouble()){
+            System.out.println("Error..please enter a valid amount");
+            scanner.next();
+        }
+        double amount =scanner.nextDouble();
+        if(amount < 0){
+            System.out.println("Value you should be positive number only ");
+            return validAmount(fromC);
+        }
+        return amount;
+    }
+}
